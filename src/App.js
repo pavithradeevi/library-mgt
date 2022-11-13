dd727dbc519d4db04af54e8bce3e5de8145b8f89
pavithradeevi <pavithradeevi@gmail.com>
2022-11-13 15:14:25 +0530

    library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react"
+import { BrowserRouter as Router } from "react-router-dom"
+import { Container } from "@mui/material"
+import { NotificationContainer } from "react-notifications"
+import { AppLayout } from "./components/layout/app-layout"
+import { UserProvider } from "./context/user-context"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => (
+  <UserProvider>
+    <Suspense fallback={null}>
+      <Container className="page-container">
+        <Router>
+          <AppLayout />
+          <NotificationContainer />
+        </Router>
+      </Container>
+    </Suspense>
+  </UserProvider>
+)
